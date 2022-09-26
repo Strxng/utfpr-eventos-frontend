c7be387c776070d5a4d4cd502984abbbcecf0f79
Vinícius <viniciusalmeidafreitas@gmail.com>
2022-09-26 03:06:40 -0300

    Created a new component Text Button

--- src/components/Buttons/index.ts
+++ src/components/Buttons/index.ts
@@ -2,5 +2,6 @@ import { PrimaryButton } from './PrimaryButton'
 import { LargeButton } from './LargeButton'
 import { OutlinedButton } from './OutlinedButton'
 import { IconButton } from './IconButton'
+import { TextButton } from './TextButton'
 
-export { PrimaryButton, LargeButton, OutlinedButton, IconButton }
+export { PrimaryButton, LargeButton, OutlinedButton, IconButton, TextButton }
