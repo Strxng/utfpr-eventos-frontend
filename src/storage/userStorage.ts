645d82a7622487a14f0be36b761559ac682b1d82
Leonardo Bazan Picinin <leonardo.picinin@ciss.com.br>
2022-11-07 23:52:48 -0300

    desacoplando asyncStorage dos storages

--- src/storage/userStorage.ts
+++ src/storage/userStorage.ts
@@ -1,14 +1,14 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import { getStorageItem, setStorageItem } from 'storage'
 
 export const findUserTokenStore = async (): Promise<string> => {
-  const userToken = await AsyncStorage.getItem('AUTH_TOKEN')
+  const userToken = await getStorageItem('AUTH_TOKEN')
   return userToken ?? ''
 }
 
 export const saveUserTokenStore = async (accessToken: string): Promise<void> => {
-  await AsyncStorage.setItem('AUTH_TOKEN', accessToken)
+  await setStorageItem('AUTH_TOKEN', accessToken)
 }
 
 export const destroyUserTokenStore = async (): Promise<void> => {
-  await AsyncStorage.setItem('AUTH_TOKEN', '')
+  await setStorageItem('AUTH_TOKEN', '')
 }
