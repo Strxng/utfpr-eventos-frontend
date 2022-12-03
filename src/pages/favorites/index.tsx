d84f659d3943466bdf7a1216b126d974827af2f3
Leonardo Bazan Picinin <leonardo.picinin@ciss.com.br>
2022-12-03 14:42:42 -0300

    mock favorite

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -4,6 +4,12 @@ import { Container, FullPage, MarginWrapper } from 'components/Wrappers'
 import React from 'react'
 import { ScrollView } from 'react-native'
 
+const mockedEvent = {
+  local: 'Local do evento',
+  nome: 'nome do evnto',
+  image: 'https://www.mirassol.sp.gov.br/imagens/16467679696227af6174875086175154.jpg'
+}
+
 export const Favorites = (): JSX.Element => {
   return (
     <FullPage spaceTop>
@@ -14,13 +20,7 @@ export const Favorites = (): JSX.Element => {
         </Container>
         <Container>
           <MarginWrapper margin={15}>
-            <FavoriteEventCard eventDate={new Date()} eventLocal={'Local do evento'} eventName={'Nome do evento'}/>
-          </MarginWrapper>
-          <MarginWrapper margin={15}>
-            <FavoriteEventCard eventDate={new Date()} eventLocal={'Local do evento'} eventName={'Nome do evento'}/>
-          </MarginWrapper>
-          <MarginWrapper margin={15} >
-            <FavoriteEventCard eventDate={new Date()} eventLocal={'Local do evento'} eventName={'Nome do evento'}/>
+            <FavoriteEventCard eventDate={new Date()} eventLocal={mockedEvent.local} eventName={mockedEvent.nome} eventImage={mockedEvent.image} />
           </MarginWrapper>
         </Container>
       </ScrollView>
