596974bea2bdc2ae5ea5251725c996c658c89e66
Leonardo Bazan Picinin <leonardo.picinin@ciss.com.br>
2022-12-03 15:45:51 -0300

    wrappers with theme

--- src/components/Wrappers/FullPage/styles.ts
+++ src/components/Wrappers/FullPage/styles.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components/native'
-import { ThemeProps } from '../../../../theme'
+import { ThemeType } from '../../../../theme'
 
 export const ContentContainer = styled.View`
   display: flex;
   min-height: 100%;
   width: 100%;
-  background-color: ${(props: ThemeProps) => props.theme.colors.background};
+  background-color: ${({ theme }: ThemeType) => theme.colors.background};
   align-items: center;
 `
